229a66c076a05a83f28b4d047bfa021c42be521e
Benjamin Bertrand <benjamin.bertrand@ess.eu>
2020-11-17 11:42:02 +0100

    Make it possible to run the app with postgres

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from .settings import SQLALCHEMY_DATABASE_URL
 
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite:"):
+    connect_args = {"check_same_thread": False}
+else:
+    connect_args = {}
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 convention = {
     "ix": "ix_%(column_0_label)s",
