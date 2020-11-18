3074d21c556f83a732de39afbd6bff4d341300ac
Benjamin Bertrand <benjamin.bertrand@ess.eu>
2020-11-18 18:07:32 +0100

    Test send_notification

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 import pytest
 import respx
 from app import schemas, utils
+from unittest import mock
 
 
 @pytest.mark.parametrize(
@@ -8,6 +9,7 @@ from app import schemas, utils
     [
         ("192.168.1.2", [], True),
         ("192.168.1.2", ["192.168.1.0/24"], True),
+        ("192.168.1", ["192.168.1.0/24"], False),
         ("192.168.1.4", ["192.168.2.0/24", "192.168.3.0/24"], False),
     ],
 )
@@ -34,3 +36,44 @@ async def test_send_push_to_ios():
     assert req.headers["apns-topic"] == "eu.ess.ESS-Notify"
     assert req.headers["Authorization"].startswith("Bearer")
     assert req._content == payload.json().encode("utf-8")
+
+
+@pytest.mark.asyncio
+async def test_send_notification(db, user_factory, notification_factory, mocker):
+    mock_send_push_to_ios = mocker.patch("app.utils.send_push_to_ios")
+    notification1 = notification_factory()
+    notification2 = notification_factory()
+    user1 = user_factory(apn_tokens=["user1-apn1", "user1-apn2"])
+    user2 = user_factory(apn_tokens=["user2-apn1"])
+    user_factory(apn_tokens=["user3-apn1"])
+    user1.notifications.append(notification1)
+    user1.notifications.append(notification2)
+    user2.notifications.append(notification1)
+    db.commit()
+    await utils.send_notification(notification1)
+    # Check that send_push_to_ios was called 3 times
+    # - twice for user1 (2 APN tokens)
+    # - once for user2 (1 APN tokens)
+    assert mock_send_push_to_ios.call_count == 3
+    user1_payload = schemas.ApnPayload(
+        aps=schemas.Aps(
+            alert=schemas.Alert(
+                title=notification1.title, subtitle=notification1.subtitle
+            ),
+            badge=2,
+        )
+    )
+    user2_payload = schemas.ApnPayload(
+        aps=schemas.Aps(
+            alert=schemas.Alert(
+                title=notification1.title, subtitle=notification1.subtitle
+            ),
+            badge=1,
+        )
+    )
+    calls = [
+        mock.call("user1-apn1", user1_payload),
+        mock.call("user1-apn2", user1_payload),
+        mock.call("user2-apn1", user2_payload),
+    ]
+    mock_send_push_to_ios.assert_has_calls(calls)
