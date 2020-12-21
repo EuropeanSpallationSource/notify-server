42587a33176d31b1c7aefdd36949e64a28eaed01
Emanuele Laface <emanuele.laface@gmail.com>
2020-12-21 11:29:02 +0100

    Fix case sensitive login

--- app/api/login.py
+++ app/api/login.py
@@ -14,16 +14,16 @@ def login(
     db: Session = Depends(deps.get_db),
     form_data: OAuth2PasswordRequestForm = Depends(),
 ):
-    if not ldap.authenticate_user(form_data.username, form_data.password):
-        logger.warning(f"LDAP authentication failed for {form_data.username}")
+    if not ldap.authenticate_user(form_data.username.lower(), form_data.password):
+        logger.warning(f"LDAP authentication failed for {form_data.username.lower()}")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
         )
-    logger.info(f"User {form_data.username} successfully logged in")
-    db_user = crud.get_user_by_username(db, form_data.username)
+    logger.info(f"User {form_data.username.lower()} successfully logged in")
+    db_user = crud.get_user_by_username(db, form_data.username.lower())
     if db_user is None:
-        db_user = crud.create_user(db, form_data.username)
+        db_user = crud.create_user(db, form_data.username.lower())
         response.status_code = status.HTTP_201_CREATED
     access_token = utils.create_access_token(db_user.username)
     return {"access_token": access_token, "token_type": "bearer"}
