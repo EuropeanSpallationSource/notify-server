fd9878cfe39eb344ef78baa94b1f2afbee41a46b
Benjamin Bertrand <beenje@gmail.com>
2022-01-03 22:43:20 +0100

    Add notifications polling
    This should be good enough for testing.
    Can be changed to SSE later.

--- app/views/notifications.py
+++ app/views/notifications.py
@@ -14,7 +14,6 @@ async def notifications_get(
     db: Session = Depends(deps.get_db),
     current_user: models.User = Depends(deps.get_current_user_from_cookie),
 ):
-    print("Get notifications")
     services = crud.get_user_services(db, current_user)
     categories = {service.id: service.category for service in services}
     selected_services = [
@@ -22,8 +21,10 @@ async def notifications_get(
         for service in services
         if service.is_subscribed
     ]
-    print(selected_services)
     notifications = crud.get_user_notifications(db, current_user)
+    request.session["selected_categories"] = [
+        service.category for service in selected_services
+    ]
     return templates.TemplateResponse(
         "notifications.html",
         {
@@ -43,8 +44,8 @@ async def notifications_post(
     current_user: models.User = Depends(deps.get_current_user_from_cookie),
 ):
     form = await request.form()
-    print(form.keys())
     selected_categories = list(form.keys())
+    request.session["selected_categories"] = selected_categories
     user_services = crud.get_user_services(db, current_user)
     selected_services = [
         schemas.UserServiceForm.from_user_service(service)
@@ -68,6 +69,31 @@ async def notifications_post(
         {
             "request": request,
             "services": selected_services,
+            "notifications": notifications,
+            "categories": categories,
+        },
+    )
+
+
+@router.get("/update", response_class=HTMLResponse, name="notifications")
+async def notifications_update(
+    request: Request,
+    db: Session = Depends(deps.get_db),
+    current_user: models.User = Depends(deps.get_current_user_from_cookie),
+):
+    user_services = crud.get_user_services(db, current_user)
+    categories = {service.id: service.category for service in user_services}
+    notifications = crud.get_user_notifications(db, current_user)
+    selected_categories = request.session.get("selected_categories", [])
+    notifications = [
+        notification
+        for notification in notifications
+        if categories[notification.service_id] in selected_categories
+    ]
+    return templates.TemplateResponse(
+        "notifications_table.html",
+        {
+            "request": request,
             "notifications": notifications,
             "categories": categories,
         },
