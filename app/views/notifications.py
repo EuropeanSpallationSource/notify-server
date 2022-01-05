8813007516d308b5127e95b49fd27069cbd4cdb5
Benjamin Bertrand <beenje@gmail.com>
2022-01-05 22:57:08 +0100

    Add notifications limit

--- app/views/notifications.py
+++ app/views/notifications.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form
 from starlette.responses import HTMLResponse
 from starlette.requests import Request
 from sqlalchemy.orm import Session
@@ -14,6 +14,11 @@ async def notifications_get(
     db: Session = Depends(deps.get_db),
     current_user: models.User = Depends(deps.get_current_user_from_cookie),
 ):
+    try:
+        notifications_limit = request.session["notifications_limit"]
+    except KeyError:
+        notifications_limit = 50
+        request.session["notifications_limit"] = notifications_limit
     services = crud.get_user_services(db, current_user)
     categories = {service.id: service.category for service in services}
     selected_services = [
@@ -21,7 +26,9 @@ async def notifications_get(
         for service in services
         if service.is_subscribed
     ]
-    notifications = crud.get_user_notifications(db, current_user)
+    notifications = crud.get_user_notifications(
+        db, current_user, limit=notifications_limit
+    )
     request.session["selected_categories"] = [
         service.category for service in selected_services
     ]
@@ -31,6 +38,7 @@ async def notifications_get(
             "request": request,
             "current_user": current_user,
             "services": selected_services,
+            "notifications_limit": notifications_limit,
             "notifications": notifications,
             "categories": categories,
         },
@@ -40,12 +48,14 @@ async def notifications_get(
 @router.post("/", response_class=HTMLResponse, name="notifications")
 async def notifications_post(
     request: Request,
+    notifications_limit: int = Form(50),
     db: Session = Depends(deps.get_db),
     current_user: models.User = Depends(deps.get_current_user_from_cookie),
 ):
     form = await request.form()
-    selected_categories = list(form.keys())
+    selected_categories = [key for key in form.keys() if key != "notifications_limit"]
     request.session["selected_categories"] = selected_categories
+    request.session["notifications_limit"] = notifications_limit
     user_services = crud.get_user_services(db, current_user)
     selected_services = [
         schemas.UserServiceForm.from_user_service(service)
@@ -64,13 +74,17 @@ async def notifications_post(
         if service.category in selected_categories
     ]
     notifications = crud.get_user_notifications(
-        db, current_user, filter_services_id=selected_services_id
+        db,
+        current_user,
+        limit=notifications_limit,
+        filter_services_id=selected_services_id,
     )
     return templates.TemplateResponse(
         "notifications_table_form.html",
         {
             "request": request,
             "services": selected_services,
+            "notifications_limit": notifications_limit,
             "notifications": notifications,
             "categories": categories,
         },
@@ -92,7 +106,10 @@ async def notifications_update(
         if service.category in selected_categories
     ]
     notifications = crud.get_user_notifications(
-        db, current_user, filter_services_id=selected_services_id
+        db,
+        current_user,
+        limit=request.session["notifications_limit"],
+        filter_services_id=selected_services_id,
     )
     return templates.TemplateResponse(
         "notifications_table.html",
